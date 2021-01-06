71445d3afb8f6be864b5a194e9f37345a5bc2ef7
Nabeel Ahmed <nabeel.ahmed@kadfire.com>
2021-01-06 01:02:25 +0000

    change some words

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome, unfortunately this is a place placeholder.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://dev.to/nabeelahmed"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out my blog, I've got something coming soon 
         </a>
       </header>
     </div>
